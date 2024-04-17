f127ea9f216466387e856b03988438dd04d64a8b
João <jfmoraeslara@gmail.com>
2024-04-17 11:09:09 -0300

    Função editar contato

--- config/process.php
+++ config/process.php
@@ -36,6 +36,33 @@
                 echo "Erro: $erro";
             }
 
+        } else if($data["type"] === "edit") {
+
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+
+            $query = "UPDATE contacts SET name = :name, phone = :phone, observations = :observations WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try {
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato editado com sucesso!";
+        
+            } catch(PDOException $e) {
+                // erro na conexão
+                $erro = $e->getMessage();
+                echo "Erro: $erro";
+            }
+
         }
 
         // REDIRECT HOME
